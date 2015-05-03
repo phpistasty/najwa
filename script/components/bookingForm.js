453b497f282fd3c2a9b725c0f588102a5728f27d
Steve Morgan <smorgan@productiveedge.com>
2015-05-03 01:53:57 -0500

    booking form up

--- script/components/bookingForm.js
+++ script/components/bookingForm.js
@@ -1,10 +1,15 @@
 (function(NS){
     "use strict";
     var modules = $('.js_bookingForm');
+    var instances = [];
 
     NS.add('bookingForm', {
         init : function(){
-
+            modules.each(function(index){
+                var form = new BookingForm(index, $(this));
+                form.bindings();
+                instances.push(form);
+            });
         }
     });
 
@@ -14,8 +19,64 @@
     }
 
     BookingForm.prototype.bindings = function(){
+        this.$el.on('focus change', '.invalid', function(){
+            NS.validations.setFieldState($(this), true);
+        });
+        this.$el.on('submit', function(evt){
 
-    }
+        });
+    };
+
+    BookingForm.prototype.submission = function(evt){
+        var formData = {
+            name: this.$el.find('.js_name').val(),
+            email: this.$el.find('.js_email').val(),
+            telephone: this.$el.find('.js_telephone').val(),
+            date: this.$el.find('.js_date').val()
+        };
+        return this.validate(formData);
+    };
+
+    BookingForm.prototype.validate =  function(formData){
+        var valid = true;
+
+        if(!NS.validations.isntEmpty(formData.name)){
+            valid = false;
+        }
+
+        if(!NS.validations.email(formData.email)){
+            this.setEmailState(false);
+            valid = false;
+        }
+
+        if(!NS.validations.phone(formData.telephone)){
+            this.setPhoneState(false);
+            valid = false;
+        }
+
+        if(!NS.validations.date.isntEmpty(formData.date)){
+            this.setDateState(false);
+            valid = false;
+        }
+
+        return valid;
+    };
+
+    BookingForm.prototype.setNameState = function(valid){
+        NS.validations.setFieldState(this.$el.find('.js_name'), valid);
+    };
+
+    BookingForm.prototype.setEmailState = function(valid){
+        NS.validations.setState(this.$el.find('.js_email'), valid);
+    };
+
+    BookingForm.prototype.setPhoneState = function(valid){
+        NS.validations.setState(this.$el.find('.js_telephone'), valid);
+    };
+
+    BookingForm.prototype.setDateState = function(valid){
+        NS.validations.setState(this.$el.find('.js_date'), valid);
+    };
 
 
 })(najwa);
--- script/lib/validations.js
+++ script/lib/validations.js
@@ -8,8 +8,17 @@
             var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
             return re.test(email);
         },
+
+        phone : function(phone){
+            phone = phone.replace(/\D/g, '');
+            return (phone.length === 10);
+        },
+
         isntEmpty : function(str){
             return str.length > 1;
+        },
+        setFieldState : function(el, state){
+            state ? el.removeClass('invalid') : el.addClass('invalid');
         }
     });
 
