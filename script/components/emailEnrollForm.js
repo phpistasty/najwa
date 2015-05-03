79c7ab035541d93f425badadab74eb46facf6502
Steve Morgan <smorgan@productiveedge.com>
2015-05-02 20:29:35 -0500

    Update, switch to VM

--- script/components/emailEnrollForm.js
+++ script/components/emailEnrollForm.js
@@ -8,12 +8,11 @@
         init : function(){
             modules.each(function(index){
                 var form = new EmailEnrollmentForm(instances.length, $(this));
-                //form.bindings();
+                form.bindings();
                 instances.push(form);
             });
         }
     });
-
 
     function EmailEnrollmentForm(index, $el){
         this.$el = $el;
@@ -38,54 +37,17 @@
     };
 
     EmailEnrollmentForm.prototype.bindings = function(){
-
+        this.$el.on('submit', function(evt){
+            evt.preventDefault();
+            evt.stopImmediatePropagation();
+            submission( this );
+            return false;
+        });
     };
 
     EmailEnrollmentForm.prototype.showSuccessState = function(){
 
     };
-
-
-    function submission(){
-        var emailAddr = modules.find('.js_emailField');
-        if(NS.validations.email(emailAddr.val())){
-            NS.xhr({
-               url : "/dummy",
-                data: {
-                    emailAddress : emailAddr.val()
-                },
-                success : successMessaging,
-                error : errorMessaging
-            });
-        }else{
-            // add validation fails
-            emailAddr.addClass('invalid');
-        }
-    }
-
-    function successMessaging(data){
-
-    }
-
-    function errorMessaging(){
-
-    }
-
-    function validationErrorMessaging(){
-
-    }
-
-    function setBindings(){
-        modules.each(function(index){
-            $(this).on('submit', function(evt){
-                evt.preventDefault();
-                evt.stopImmediatePropagation();
-                submission( this );
-                return false;
-            });
-        });
-    }
-
 
 
 })(najwa);
